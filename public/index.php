9a77d3127e177d622ac640ea999666ea3259a6ac
Mikita Shydlouski <retdarkw@gmail.com>
2024-05-20 12:26:14 +0300

    add gitignore into root

--- public/index.php
+++ public/index.php
@@ -7,14 +7,7 @@ use src\Animal\Bird;
 use src\Animal\Cat;
 
 spl_autoload_register(function ($class_name) {
-    //Почему это работает?
     $global_path = sprintf('%s.php', str_replace('\\', DIRECTORY_SEPARATOR, $class_name));
-
-    // //а это нет
-    // $global_path2 = sprintf('%s.php', $class_name);
-
-    // // а тутт понятно.., бред ведь))
-    // echo sprintf('%s != %s <br>', $global_path, $global_path2);
 
     if (file_exists($global_path)) {
         include $global_path;
